1166ea1c5bddca23e4c33a593550027f8522f7b6
Christian Ortega <csortega@iucesmag.edu.co>
2018-05-02 14:07:02 -0500

    Archivos iniciales

---
+++ WordBook/app/src/main/java/com/sime/itanortegaortega/wordbook/CategoriesActivity.java
@@ -0,0 +1,13 @@
+package com.sime.itanortegaortega.wordbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
---
+++ WordBook/app/src/main/java/com/sime/itanortegaortega/wordbook/WordsActivity.java
@@ -0,0 +1,13 @@
+package com.sime.itanortegaortega.wordbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_words);
+    }
+}
