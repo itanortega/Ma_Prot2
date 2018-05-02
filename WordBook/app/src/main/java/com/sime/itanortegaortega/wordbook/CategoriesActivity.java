ad03a06cd53aeacd4ffa7a02c8172ad46f4cfd08
Christian Ortega <csortega@iucesmag.edu.co>
2018-05-02 17:03:34 -0500

    Categorias y palabras dinámicas

--- WordBook/app/src/main/java/com/sime/itanortegaortega/wordbook/CategoriesActivity.java
+++ WordBook/app/src/main/java/com/sime/itanortegaortega/wordbook/CategoriesActivity.java
@@ -1,13 +1,97 @@
 package com.sime.itanortegaortega.wordbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
-public class CategoriesActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CategoriesActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private static String LOCAL = "";
+    private CategoriasAdapter adapter;
+    private GridView Gv_Categorias;
+    private ExecutorService queue = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        Gv_Categorias =  (GridView)findViewById(R.id.Gv_Categorias);
+        Gv_Categorias.setOnItemClickListener(this);
+
+        LOCAL = getApplicationContext().getFilesDir().getAbsolutePath() + "/";
+
+        cargarListaCategorias();
+    }
+
+    private void cargarListaCategorias() {
+        final String strUrl_Local = LOCAL + "words.json";
+
+        final Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                CAFData data = null;
+                data = CAFData.dataWithContentsOfFile(strUrl_Local);
+                try {
+                    JSONObject root = new JSONObject(data.toText());
+                    JSONArray catJson = root.getJSONArray("categorias");
+
+                    final JSONArray categoriasJson = catJson;
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayList<Categoria> categorias = new ArrayList<>();
+                            Categoria c;
+                            JSONObject categoriaJson;
+
+                            for (int i = 0; i < categoriasJson.length(); i++) {
+                                try {
+                                    categoriaJson = categoriasJson.getJSONObject(i);
+                                    c = new Categoria(i, categoriaJson.getString("nombre").toString(), "", "");
+                                    categorias.add(c);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            final ArrayList<Categoria> categoriasV = categorias;
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    adapter = new CategoriasAdapter(CategoriesActivity.this, categoriasV);
+                                    Gv_Categorias.setAdapter(adapter);
+                                }
+                            });
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        queue.execute(thread);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) { Log.d("debugapp", "clic");
+        Categoria item = (Categoria) parent.getItemAtPosition(position);
+
+        Intent intent = new Intent(this, WordsActivity.class);
+        intent.putExtra("id_categoria", item.getId());
+        startActivity(intent);
     }
 }
--- WordBook/app/src/main/java/com/sime/itanortegaortega/wordbook/WordsActivity.java
+++ WordBook/app/src/main/java/com/sime/itanortegaortega/wordbook/WordsActivity.java
@@ -2,12 +2,88 @@ package com.sime.itanortegaortega.wordbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class WordsActivity extends AppCompatActivity {
+
+    private static String LOCAL = "";
+
+    int id_categoria;
+    RecyclerView Rv_Palabras;
+    LinearLayoutManager linearLayoutManager;
+    private PalabrasAdapter palabrasAdapter;
+    private ExecutorService queue = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_words);
+        id_categoria = (int) getIntent().getIntExtra("id_categoria", 0);
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(linearLayoutManager.VERTICAL);
+
+        Rv_Palabras = (RecyclerView) findViewById(R.id.Rv_Palabras);
+        Rv_Palabras.setLayoutManager(linearLayoutManager);
+
+        LOCAL = getApplicationContext().getFilesDir().getAbsolutePath() + "/";
+
+        cargarPalabras();
+    }
+
+    public void cargarPalabras(){
+        Runnable thread = new Runnable() {
+            @Override
+            public void run() {
+                String strUrl = LOCAL + "words.json";
+                CAFData data = null;
+
+                data = CAFData.dataWithContentsOfFile(strUrl);
+
+                try {
+                    JSONObject root = new JSONObject(data.toText());
+                    JSONArray catJson = root.getJSONArray("categorias");
+                    JSONObject categoriaJson = catJson.getJSONObject(id_categoria);
+                    JSONArray palabras = categoriaJson.getJSONArray("words");
+
+                    final JSONArray palabrasJson = palabras;
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ArrayList<Palabra> palabras = new ArrayList<>();
+                            Palabra p;
+                            JSONObject palabraJson;
+
+                            for(int i=0; i<palabrasJson.length(); i++){
+                                try {
+                                    palabraJson = palabrasJson.getJSONObject(i);
+                                    p = new Palabra(i, "", palabraJson.getString("inglés").toString());
+                                    palabras.add(p);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            palabrasAdapter = new PalabrasAdapter(palabras, getApplicationContext());
+                            Rv_Palabras.setAdapter(palabrasAdapter);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        queue.execute(thread);
     }
 }
